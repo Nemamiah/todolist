0f563b32be1d2962e90be66979fbe010e4ec50d5
Nemamiah <thibault.bouche@gmail.com>
2021-10-25 21:16:12 +0200

    JSinit

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-// bouton traité validé. https://image.flaticon.com/icons/png/512/1250/1250185.png
+console.log("Hello");
+
+function searchBar(){
+    console.log("searchbar ok")
+}
+
+function addTask(){
+    console.log("addtask ok");
+}
+
+function openNotif(){
+    console.log("notif ok");
+}
+
+function openProfil(){
+    console.log("profil ok");
+}
+
+function deconnect(){
+    console.log("deconnect ok");
+}
+
+function shortWay(){
+    console.log("shortWay ok");
+}
+
+function reloadAccueil(){
+    console.log("reload accueil ok");
+}
+
+function leftButton(){
+    console.log("left button ok");
+}
+
+function rightButton(){
+    console.log("right button ok");
+}
+
+function deleteTask(){
+    console.log("deleteTask ok");
+}
+
+function doneTask(){
+    console.log("doneTask ok");
+}
+
